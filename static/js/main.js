6a51d9f1088fdc3c6ffe9d96788cce177fd180fb
memorandumtk <memorandum876@outlook.com>
2024-04-11 23:46:51 -0700

    Just after reciving the Iino-san review and before fixing those review points.

--- static/js/main.js
+++ static/js/main.js
@@ -6,11 +6,13 @@ let note = document.getElementById("note");
 
 let formulaValue = ""; // 計算式の値
 let resultValue = ""; // 答えの値
-let isFirstFormula = true; // 最初の計算式かどうかのBoolean値
-let isFirstString = true; // 最初の計算式かどうかのBoolean値
+let isFirstFormula = true; // Boolean: 最初の計算式かどうか
+let isFirstString = true; // Boolean: 最初の計算式かどうか
+let historyOfFormula = []; // Array: 計算式の履歴
+//[0: {formula: "1+2", result: "3"}, 1: {formula: "3+4", result: "7"}..]
 
 // ボタンの値の定数
-const OPERATORS = ["/", "*", "+", "-", "%"];
+const OPERATORS = ["÷", "×", "+", "-", "%"];
 const DECIMAL_POINT = ".";
 const EQUAL = "=";
 const ALL_CLEAR = "AC";
@@ -22,26 +24,29 @@ const ACButtonIsClicked = () => {
     formulaValue = "";
     resultValue = "";
     result.innerText = resultValue;
-    note.value += "Cleared\n"; // Note that the calculator was cleared
+    note.value += "Cleared\n";
+    isFirstString = true;
+    isFirstFormula = true;
 }
 
 const EqualButtonIsClicked = () => {
     console.log('EqualButtonIsClicked')
     try {
-        isFirstFormula = false;
         resultValue = eval(formulaValue);
         result.innerText = resultValue;
         formulaValue = resultValue;
-        numbers.map((numEle) => {
-            numEle.disabled = true;
-        });
+        // numbers.map((numEle) => {
+        //     numEle.disabled = true;
+        // });
         note.value += ` = ${resultValue}\n`; // Noteに計算結果を追加、イコールサインと改行を含め。
     } catch {
         result.innerText = "ERROR.";
         resultValue = "";
         formulaValue = "";
-        note.value += "Error\n"; // Note an error occurred
+        note.value += "Error\n";
     }
+    isFirstFormula = false;
+    isFirstString = true;
 }
 
 const OperatorButtonIsClicked = (enteredButtonText) => {
@@ -49,6 +54,7 @@ const OperatorButtonIsClicked = (enteredButtonText) => {
     formulaValue += enteredButtonText;
     formula.innerText = formulaValue;
     note.value = formulaValue; // Noteに計算式を追加
+    isFirstString = false;
 }
 
 const DecimalPointButtonIsClicked = (enteredButtonText) => {
@@ -56,6 +62,7 @@ const DecimalPointButtonIsClicked = (enteredButtonText) => {
     formulaValue += enteredButtonText;
     formula.innerText = formulaValue;
     note.value = formulaValue; // Noteに計算式を追加
+    isFirstString = false;
 }
 
 const NumberButtonIsClicked = (enteredButtonText) => {
@@ -63,35 +70,42 @@ const NumberButtonIsClicked = (enteredButtonText) => {
     formulaValue += enteredButtonText;
     formula.innerText = formulaValue;
     note.value = formulaValue; // Noteに計算式を追加
+    isFirstString = false;
 }
+
 const ButtonIsClicked = (enteredButtonText) => {
     console.log(`isFirstFormula? ${isFirstFormula}`)
     console.log(`isFirstString? ${isFirstString}`)
 
     if (enteredButtonText === ALL_CLEAR) {
-        // All-Clearボタンが押された場合
+        // All-Clearボタンが押された場合の処理
         ACButtonIsClicked();
     } else if (enteredButtonText === EQUAL) {
-        // Equalボタンが押された場合
+        // Equalボタンが押された場合の処理
         if (isFirstString) {
-        } else if (!isFirstString){
+        } else if (!isFirstString) {
             EqualButtonIsClicked();
         }
     } else if (OPERATORS.includes(enteredButtonText)) {
-        // 演算子ボタンが押された場合
-        if (isFirstString) {
-        } else if (!isFirstString){
+        // 演算子ボタンが押された場合の処理
+        if (isFirstString && !isFirstFormula) {
+            OperatorButtonIsClicked(enteredButtonText);
+        } else if (!isFirstString) {
             OperatorButtonIsClicked(enteredButtonText);
         }
     } else if (enteredButtonText === DECIMAL_POINT) {
-        // 演算子ボタンが押された場合
+        // 演算子ボタンが押された場合の処理
         if (isFirstString) {
-        } else if (!isFirstString){
+        } else if (!isFirstString) {
             DecimalPointButtonIsClicked(enteredButtonText);
         }
     } else {
-        // 数字ボタンが押された場合
-        NumberButtonIsClicked(enteredButtonText, isFirstFormula);
+        // 数字ボタンが押された場合の処理
+        if (isFirstString && isFirstFormula) {
+            NumberButtonIsClicked(enteredButtonText);
+        } else if (!isFirstString) {
+            NumberButtonIsClicked(enteredButtonText);
+        }
     }
 
     console.log(formulaValue);
