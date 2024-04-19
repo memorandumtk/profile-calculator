90019f23c58079ec9e65b78ddb7ea0950e7fbf39
memorandumtk <memorandum876@outlook.com>
2024-04-18 23:53:06 -0700

    2024/04/19 commit Added a function to transfer sign and fixed problems of clear button and dot button.

---
+++ static/js/calculate copy.js
@@ -0,0 +1,173 @@
+import { isWriteToNoteEnabled } from "./note.js";
+
+let formulaDiv = document.getElementById("formula");
+let resultDiv = document.getElementById("result");
+let noteUl = document.getElementById("note-ul");
+
+// 定数
+const OPERATORS = ["÷", "×", "+", "-", "%"];
+const DECIMAL_POINT = ".";
+const EQUAL = "=";
+const ALL_CLEAR = "AC";
+const CLEAR = "C";
+const TRANSFER_OPERATOR = "+/-";
+
+// グローバル変数
+let currentInput = ""; // 入力された値を保持する変数（1文字）
+let resultValue = ""; // 計算結果を保持する変数
+let previeousInput = ""; // 直前の式を保持する変数
+let calculationHistory = []; // 計算履歴を保持する変数: Array
+let counter = 0; // 計算履歴のカウンターで履歴のインデックスとしても使用する
+let item = {}; // 計算式と答えと項の履歴を保持する変数: Object
+let terms = []; // 項の履歴を保持する変数: Array
+let term = ""; // 項を保持する変数
+
+// 計算式と答え部分を更新する関数
+function updateDisplay() {
+    formulaDiv.innerText = currentInput || "0";
+    resultDiv.innerText = resultValue;
+}
+
+// ノートに追加するリストを作成する関数
+function addToNote (item) {
+    let noteLi = document.createElement("li");
+    noteLi.innerText = item.formula + " = " + item.result;
+    noteUl.appendChild(noteLi);
+}
+
+// 項を構成する関数
+function constructTerm(term, currentInput) {
+    if (term) {
+    }
+    return term;
+}
+
+// 履歴Arrayに計算式と答えが入ったJSONを格納する関数
+function addToHistory(formula, term = "", result = "") {
+    terms = constructTerm(terms, term); 
+    item = { formula: formula, terms: terms, result: result };
+    calculationHistory[counter] = item;
+    if (isWriteToNoteEnabled) {
+        addToNote(item);
+    }
+    console.log(...calculationHistory)
+}
+
+// 履歴Arrayをリセットする関数
+function resetHistory() {
+    calculationHistory = [];
+    noteUl.innerHTML = "";
+}
+
+
+// ACボタンが押された場合の処理
+function handleAllClear() {
+    currentInput = "";
+    resultValue = "";
+    updateDisplay();
+    addToHistory({ formula: "Cleared" });
+}
+
+// Cボタンが押された場合の処理
+function handleClear() {
+    currentInput = currentInput.slice(0, -1);
+    updateDisplay();
+}
+
+// =ボタンが押された場合の処理
+function handleEqual(equalSign) {
+    try {
+        resultValue = eval(currentInput); // Consider a safer math library for production
+        addToHistory(currentInput, equalSign, resultValue);
+    } catch (error) {
+        console.log(error);
+        resultValue = "ERROR";
+    }
+    previeousInput = resultValue; // Set new input after calculation
+    console.log(resultValue)
+    updateDisplay();
+    resultValue = "";
+    currentInput = "";
+    counter++;
+}
+
+
+// 演算子が押された場合の処理
+function handleOperators(operator) {
+    // ×記号と÷記号を*と/に変換、計算のため
+    if (operator === "÷") {
+        operator = "/";
+    } else if (operator === "×") {
+        operator = "*";
+    }
+
+    if (currentInput === "" && operator === "-" && previeousInput === "") {
+        currentInput += operator;
+        updateDisplay();
+        return;
+    }
+
+    if (currentInput === "" && previeousInput === "") {
+        return;
+    }
+
+    if (currentInput === "" && previeousInput) {
+        currentInput = previeousInput;
+    }
+
+    if (currentInput) { // Guard against operator at the beginning
+        currentInput += operator;
+        // const formulaToBeAdded = { formula: currentInput, terms: currentInput, result: "" };
+        addToHistory(currentInput, operator, "");
+    }
+    updateDisplay();
+}
+
+// 小数点が押された場合の処理
+function handleDecimalPoint() {
+    if (currentInput.indexOf(DECIMAL_POINT) === -1) {
+        currentInput += DECIMAL_POINT;
+        updateDisplay();
+    }
+}
+
+// 数字が押された場合の処理
+function handleNumbers(number) {
+    if (resultValue && !previeousInput) { // 計算式が残っていたらリセットする
+        currentInput = "";
+        resultValue = "";
+        updateDisplay();
+    }
+    currentInput = number;
+    addToHistory(currentInput, number, "");
+    updateDisplay();
+    previeousInput = "";
+}
+
+// 押されたボタンによって処理を分岐
+function handleButtonClick(buttonText) {
+    if (buttonText === ALL_CLEAR) {
+        handleAllClear();
+    } else if (buttonText === CLEAR) {
+        handleClear();
+    } else if (buttonText === EQUAL) {
+        handleEqual(buttonText);
+    } else if (OPERATORS.includes(buttonText)) {
+        handleOperators(buttonText);
+    } else if (buttonText === DECIMAL_POINT) {
+        handleDecimalPoint();
+    } else {
+        // 数字が押された場合
+        handleNumbers(buttonText);
+    }
+}
+
+
+// 計算機のボタンにEvent Listenerを付ける
+document.querySelectorAll("#calculator-btns .btn").forEach((button) => {
+    button.addEventListener("click", () => {
+        handleButtonClick(button.innerText);
+    });
+});
+
+export { handleButtonClick, updateDisplay, resetHistory };
--- static/js/calculate.js
+++ static/js/calculate.js
@@ -10,38 +10,77 @@ const DECIMAL_POINT = ".";
 const EQUAL = "=";
 const ALL_CLEAR = "AC";
 const CLEAR = "C";
+const TRANSFER_OPERATOR = "+/-";
 
 // グローバル変数
 let currentInput = ""; // 入力された値を保持する変数（1文字）
+let currentFormula = []; // 計算式を保持する変数: Array
 let resultValue = ""; // 計算結果を保持する変数
-let previeousInput = ""; // 直前の式を保持する変数
+let previeousResultValue = ""; // 直前の結果を保持する変数
 let calculationHistory = []; // 計算履歴を保持する変数: Array
 let counter = 0; // 計算履歴のカウンターで履歴のインデックスとしても使用する
 let item = {}; // 計算式と答えと項の履歴を保持する変数: Object
-let terms = []; // 項の履歴を保持する変数: Array
+let currentTerms = []; // 項の履歴を保持する変数: Array
 let term = ""; // 項を保持する変数
 
-// 計算式と答え部分を更新する関数
-function updateDisplay() {
-    formulaDiv.innerText = currentInput || "0";
-    resultDiv.innerText = resultValue;
-}
-
 // ノートに追加するリストを作成する関数
-function addToNote (item) {
+function addToNote () {
     let noteLi = document.createElement("li");
-    noteLi.innerText = item.formula + " = " + item.result;
+    if (resultValue === "ERROR") {
+
+    } else if (resultValue === "") {
+        noteLi.innerText = currentFormula.join("");
+    } else if (resultValue) {
+        noteLi.innerText = currentFormula.join("") + " = " + resultValue;
+    }
     noteUl.appendChild(noteLi);
 }
 
-// 履歴Arrayに計算式と答えが入ったJSONを格納する関数
-function addToHistory(formula, term = "", result = "") {
-    terms.push(term);
-    item = { formula: formula, terms: terms, result: result };
-    calculationHistory[counter] = item;
+/**
+ * 計算式と答え部分を更新する関数
+ */
+function updateDisplay() {
+    formulaDiv.innerText = currentFormula.join("") || "0";
+    resultDiv.innerText = resultValue;
+
     if (isWriteToNoteEnabled) {
-        addToNote(item);
+        addToNote();
     }
+}
+
+// 項を構成する関数
+function constructTerm(currentInput) {
+    console.log(currentInput.match(/\d+/))
+    if (currentInput.match(/\d/) && term.match(/-?[0-9]+/)) {
+        term += currentInput;
+    } else if (currentInput.match(/\d/) && term.match(/-?[0-9]+\./)) {
+        term += currentInput;
+        return;
+    } else if (currentInput === ".") {
+        term += currentInput;
+        currentTerms.pop();
+        return;
+    } else if (currentInput === TRANSFER_OPERATOR && term.match(/-?[0-9]+/)) {
+        currentTerms[currentTerms.length - 1] = term * -1;
+        return;
+    } else {
+        term = currentInput;
+    }
+    currentTerms.push(term);
+    return currentTerms;
+}
+
+// 履歴Arrayに計算式と答えが入ったJSONを格納する関数
+function addToHistory(currentInput, result = "") {
+    constructTerm(currentInput);
+
+    if (currentInput === TRANSFER_OPERATOR || currentInput === EQUAL) {
+    } else {
+        currentFormula.push(currentInput);
+    }
+
+    item = { formula: currentFormula, terms: currentTerms, result: result };
+    calculationHistory[counter] = item;
     console.log(...calculationHistory)
 }
 
@@ -54,7 +93,8 @@ function resetHistory() {
 
 // ACボタンが押された場合の処理
 function handleAllClear() {
-    currentInput = "";
+    currentFormula = [];
+    currentTerms = [];
     resultValue = "";
     updateDisplay();
     addToHistory({ formula: "Cleared" });
@@ -62,95 +102,96 @@ function handleAllClear() {
 
 // Cボタンが押された場合の処理
 function handleClear() {
-    currentInput = currentInput.slice(0, -1);
+    currentFormula.pop();
+    currentTerms[currentTerms.length - 1].length > 1 ? currentTerms[currentTerms.length - 1].slice(0, -1) : currentTerms.pop();
     updateDisplay();
 }
 
 // =ボタンが押された場合の処理
-function handleEqual(equalSign) {
+function handleEqual(currentInputEqualSign) {
     try {
-        resultValue = eval(currentInput); // Consider a safer math library for production
-        addToHistory(currentInput, equalSign, resultValue);
+        resultValue = eval(currentTerms.join("")); // Consider a safer math library for production
+        addToHistory(currentInputEqualSign, resultValue);
     } catch (error) {
         console.log(error);
         resultValue = "ERROR";
     }
-    previeousInput = resultValue; // Set new input after calculation
-    console.log(resultValue)
+
+    previeousResultValue = resultValue; // Set new input after calculation
     updateDisplay();
     resultValue = "";
-    currentInput = "";
+    currentFormula = [];
+    currentTerms = [];
     counter++;
 }
 
+
 // 演算子が押された場合の処理
-function handleOperators(operator) {
+function handleOperators(currentInputOperator) {
     // ×記号と÷記号を*と/に変換、計算のため
-    if (operator === "÷") {
-        operator = "/";
-    } else if (operator === "×") {
-        operator = "*";
+    if (currentInputOperator === "÷") {
+        currentInputOperator = "/";
+    } else if (currentInputOperator === "×") {
+        currentInputOperator = "*";
     }
 
-    if (currentInput === "" && operator === "-" && previeousInput === "") {
-        currentInput += operator;
-        updateDisplay();
+    if (currentFormula === "" && previeousResultValue === "") {
         return;
     }
 
-    if (currentInput === "" && previeousInput === "") {
-        return;
+    if (currentFormula === "" && previeousResultValue) {
+        currentFormula = previeousResultValue;
     }
 
-    if (currentInput === "" && previeousInput) {
-        currentInput = previeousInput;
-    }
-
-    if (currentInput) { // Guard against operator at the beginning
-        currentInput += operator;
-        // const formulaToBeAdded = { formula: currentInput, terms: currentInput, result: "" };
-        addToHistory(currentInput, operator, "");
+    if (currentFormula) { // Guard against currentInputOperator at the beginning
+        addToHistory(currentInputOperator);
     }
     updateDisplay();
 }
 
 // 小数点が押された場合の処理
-function handleDecimalPoint() {
-    if (currentInput.indexOf(DECIMAL_POINT) === -1) {
-        currentInput += DECIMAL_POINT;
+function handleDecimalPoint(currentInputDecimalPoint) {
+    if (term.indexOf(currentInputDecimalPoint) === -1) {
+        addToHistory(currentInputDecimalPoint);
         updateDisplay();
     }
 }
 
-// 数字が押された場合の処理
-function handleNumbers(number) {
-    if (resultValue && !previeousInput) { // 計算式が残っていたらリセットする
-        currentInput = "";
-        resultValue = "";
-        updateDisplay();
-    }
-    currentInput += number;
-    addToHistory(currentInput, number, "");
+// +-切り替えボタンが押された場合の処理
+function handleTransferOperator(currentInputTransferOperator) {
+    addToHistory(currentInputTransferOperator);
     updateDisplay();
-    previeousInput = "";
+}
+
+// 数字が押された場合の処理
+function handleNumbers(currentInputNumber) {
+    if (resultValue && !previeousResultValue) { // 計算式が残っていたらリセットする
+        resultValue = "";
+    }
+    addToHistory(currentInputNumber);
+    updateDisplay();
+    previeousResultValue = "";
 }
 
 // 押されたボタンによって処理を分岐
 function handleButtonClick(buttonText) {
-    if (buttonText === ALL_CLEAR) {
+    currentInput = buttonText;
+    if (currentInput === ALL_CLEAR) {
         handleAllClear();
-    } else if (buttonText === CLEAR) {
+    } else if (currentInput === CLEAR) {
         handleClear();
-    } else if (buttonText === EQUAL) {
-        handleEqual(buttonText);
-    } else if (OPERATORS.includes(buttonText)) {
-        handleOperators(buttonText);
-    } else if (buttonText === DECIMAL_POINT) {
-        handleDecimalPoint();
-    } else {
-        // 数字が押された場合
-        handleNumbers(buttonText);
+    } else if (currentInput === EQUAL) {
+        handleEqual(currentInput);
+    } else if (OPERATORS.includes(currentInput)) {
+        handleOperators(currentInput);
+    } else if (currentInput === DECIMAL_POINT) {
+        handleDecimalPoint(currentInput);
+    } else if (currentInput === TRANSFER_OPERATOR) {
+        handleTransferOperator(currentInput);
+    } else if (currentInput.match(/[0-9]/)){ // 数字が押された場合
+        handleNumbers(currentInput);
     }
+    
 }
 
 
