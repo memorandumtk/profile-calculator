e21de0d252f954149d337f90f50c9693d6547a65
memorandumtk <memorandum876@outlook.com>
2024-05-30 23:32:26 -0400

    2024/5/31 commit 05 Fixed the way to calculate a multiplication mixed with minus.

--- static/js/calculate.js
+++ static/js/calculate.js
@@ -239,35 +239,40 @@ function handleTransferOperator(currentInputTransferOperator) {
 
     console.log('curentTerms', term)
     console.log('currentTerms', currentTerms)
-    // 直前の項をマイナス変換。currentTerms用
-    currentTerms[currentTerms.length - 1] = term * -1;
-    // 最後の数字をマイナス変換し、+か-の記号に当たればその符号を切り替え。currentFormula用
-    console.log('currentFormula')
-    console.log(currentFormula);
+    // 対象の項を取得する。+か-の記号に当たればその符号を切り替え。currentFormula用
     let temp = currentFormula.pop();
     while (currentFormula.length > 0) {
         if (currentFormula[currentFormula.length - 1].match(/[\+\-\*\/]/)) {
+            if (currentFormula[currentFormula.length - 2].match(/[\+\-\*\/]/)) {
+                temp = currentFormula.pop() + temp;
+            }
             break;
         }
         temp = currentFormula.pop() + temp;
     }
-    // if (currentFormula.length > 0 && currentFormula[currentFormula.length - 1].match(/[\+]/)) {
-    //     currentFormula.pop();
-    // } else if (currentFormula.length > 0 && currentFormula[currentFormula.length - 1].match(/[\-]/)) {
-    //     currentFormula[currentFormula.length - 1] = "+";
-    // }
+
     // tempをマイナス変換してcurrentFormulaに追加。tempが複数の数字を持つ場合を考慮し、joinで1文字に分割後、再度結合。
-    term = temp * -1;
-    let concatenatedTemp = term.toString().split("");
+    temp = temp * -1;
+    // 返還後の項をconcatenatedTempに格納し、currentFormulaに追加
+    let concatenatedTemp = temp.toString().split("");
     if (currentFormula.length > 0 && currentFormula[currentFormula.length - 1].match(/\+/) && concatenatedTemp[0] === "-") {
         currentFormula.pop();
-    } else if (currentFormula.length > 0 && currentFormula[currentFormula.length - 1].match(/\-/) && concatenatedTemp[0] === "-"){
+    } else if (currentFormula.length > 0 && currentFormula[currentFormula.length - 1].match(/\-/) && concatenatedTemp[0] === "-") {
         currentFormula[currentFormula.length - 1] = "+";
-        concatenatedTemp = concatenatedTemp.slice(1);
+        concatenatedTemp.shift();
+        // concatenatedTemp = concatenatedTemp.slice(1);
+    } else if (currentFormula.length > 0 && currentFormula[currentFormula.length - 1].match(/[\*\/]/) && concatenatedTemp[0] === "+") {
+        concatenatedTemp.shift();
+    } else if (currentFormula.length > 0 && currentFormula[currentFormula.length - 1].match(/[\*\/]/) && concatenatedTemp[0] === "-") {
+        concatenatedTemp.unshift('(');
+        concatenatedTemp.push(')');
     }
-    currentFormula.push(...concatenatedTemp);
+    concatenatedTemp = concatenatedTemp.join("");
+    currentFormula.push(concatenatedTemp);
     console.log('currentFormula After')
     console.log(currentFormula);
+    term = (concatenatedTemp[0] === "+") ? concatenatedTemp : "+" + concatenatedTemp;
+    currentTerms[currentTerms.length - 1] = term;
 
     addToHistory(currentInputTransferOperator);
     updateDisplay();
